d2f4b937a22ed909544f4786dc9682db9b6ca6c8
Jcampbell-cyber <J_ncampbell@hotmail.com>
2026-01-11 14:58:29 +1100

    Update gtag.ts

--- src/gtag.ts
+++ src/gtag.ts
@@ -1,6 +1,7 @@
 export const pageview = (url: string, title: string) => {
-  if (window.gtag) {
+  if (typeof window !== 'undefined' && window.gtag) {
     window.gtag('event', 'page_view', {
+      page_location: window.location.href,
       page_path: url,
       page_title: title,
     });
