5b93d8c94997390d654213d4b0058fba6a2a169e
Jcampbell-cyber <J_ncampbell@hotmail.com>
2026-01-11 14:48:42 +1100

    Update import path for Navigation component

--- src/GAListener.tsx
+++ src/GAListener.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { pageview } from './gtag';
-import { pages } from './Navigation'; // adjust if in same folder
+import { pages } from './components/Navigation'; // adjust if in same folder
 
 export function GAListener() {
   const location = useLocation();
