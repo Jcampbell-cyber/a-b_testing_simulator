95646f4aa9cf671704bc0009c2ad02336f7d65c9
Jcampbell-cyber <J_ncampbell@hotmail.com>
2026-01-11 14:27:59 +1100

    Refactor GAListener placement in main.tsx
    Moved GAListener component inside BrowserRouter for proper routing context.

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
+import App from "./App.tsx"; // your main App component with Routes
 import "./index.css";
 import { GAListener } from './GAListener';
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      {/* GAListener must be inside BrowserRouter */}
+      <GAListener />
       <App /> 
     </BrowserRouter>
   </React.StrictMode>
 );
-
-function App() {
-  return (
-    <BrowserRouter>
-      <GAListener />
-      <Routes>
-        {/* your routes here */}
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
