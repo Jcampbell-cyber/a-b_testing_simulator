b95464e2c1a723d2cc5ad6f948b21847df480bd8
Jcampbell-cyber <J_ncampbell@hotmail.com>
2026-01-25 17:11:11 +1100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx"; // your main App component with Routes
 import "./index.css";
 import { GAListener } from './GAListener';
+import { HelmetProvider } from 'react-helmet-async'; // <-- add this
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      {/* GAListener must be inside BrowserRouter */}
-      <GAListener />
-      <App /> 
-    </BrowserRouter>
+    <HelmetProvider> {/* <-- wrap your app with this */}
+      <BrowserRouter>
+        {/* GAListener must be inside BrowserRouter */}
+        <GAListener />
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
