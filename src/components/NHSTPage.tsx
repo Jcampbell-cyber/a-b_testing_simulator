f1a2a28edc0550e3e98f8508fca33d842ec560bf
Jcampbell-cyber <J_ncampbell@hotmail.com>
2026-01-25 21:57:19 +1100

    Updated NHSTPage.tsx

--- src/components/NHSTPage.tsx
+++ src/components/NHSTPage.tsx
@@ -577,10 +577,11 @@ const NHSTPage: React.FC = () => {
               >
                 Explore False Positives from Peeking →
               </Link>
-            </div> 
+            </div>
+          </div> {/* closes bg-gray-800 rounded-xl wrapper */}
         </div> {/* closes max-w-7xl */}
       </div> {/* closes min-h-screen */}
-  );
+    );
 };
 
 export default NHSTPage;
