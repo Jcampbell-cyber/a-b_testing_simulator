c880d146383730c53fd80f431836d30cae3e03b9
Jcampbell-cyber <J_ncampbell@hotmail.com>
2026-04-11 21:05:15 +1000

    Update BootstrapMeanPage.tsx

--- src/components/BootstrapMeanPage.tsx
+++ src/components/BootstrapMeanPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
-
+import { Link } from 'react-router-dom';
 import { runBootstrapMean } from './bootstrapMeanSimulation';
 
 import { BootstrapMeanChart } from './BootstrapMeanChart';
