dba898cc3d4ea3e785ad6c083f52fbada54ade89
Jcampbell-cyber <J_ncampbell@hotmail.com>
2026-09-22 21:48:25 +1000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,26 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.tsx"; // your main App component with Routes
-import "./index.css";
-import { GAListener } from './GAListener';
-import { HelmetProvider } from 'react-helmet-async'; // <-- add this
-
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <HelmetProvider> {/* <-- wrap your app with this */}
-      <BrowserRouter>
-        {/* GAListener must be inside BrowserRouter */}
-        <GAListener />
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
-  </React.StrictMode>
-);
+import React from "react";  
+import { createRoot } from "react-dom/client";  
+import { BrowserRouter } from "react-router-dom";  
+import App from "./App.tsx";  
+import "./index.css";  
+import { GAListener } from './GAListener';  
+import { HelmetProvider } from 'react-helmet-async';  
+import posthog from 'posthog-js';  
+import { PostHogProvider } from 'posthog-js/react';
+ 
+posthog.init('phc_kJqyofaSXhRqdD9cW768tjFaYFm2A9db3amwaudL74Gd', {  
+ api_host: 'https://eu.i.posthog.com',  
+});
+ 
+createRoot(document.getElementById("root")!).render(  
+ <React.StrictMode>  
+ <PostHogProvider client={posthog}>  
+ <HelmetProvider>  
+ <BrowserRouter>  
+ <GAListener />  
+ <App />  
+ </BrowserRouter>  
+ </HelmetProvider>  
+ </PostHogProvider>  
+ </React.StrictMode>  
+);  
